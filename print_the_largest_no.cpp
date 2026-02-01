ef2b7622b133dd241371fe3fe6dfe927578cc1c8
Ritik Dawande <ritikdawande25@gmail.com>
2026-02-01 15:10:28 +0530

    Add files via upload

---
+++ print_the_largest_no.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+
+    cout<<"your 1st no. = " ;
+
+    cin>>a;
+    cout<<"your 2nd no. =" ;
+    cin>>b;
+    if (a>b){
+        cout<<"a is greater ="<<" "<<a;
+    }
+    else{
+        cout<<"b is greater = "<<" " <<b;
+    }
+    return 0;
+}
