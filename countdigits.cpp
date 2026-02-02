c0dfbca6eb81ab0e7fa322709f5d06a9837e0cde
Ritik Dawande <ritikdawande25@gmail.com>
2026-02-02 20:16:45 +0530

    Add files via upload

---
+++ countdigits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n; // Number enter by user
+    cout << "Enter a number :";
+    cin>>n;
+    int count =0;
+    while (n>0)
+    {
+         int lastdigit = n%10;
+         count++;
+         n=n/10;
+
+
+    }
+    cout<<count;
+
+}
